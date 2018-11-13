1824ea144dd76136b559a131018a91e2b2798a07
NorcyChen <norcychen@tencent.com>
2018-11-13 10:47:56 +0800

    Auto Commit

--- GitPush.sh
+++ GitPush.sh
@@ -1,8 +1,10 @@
-commitMessage = "Auto Commit"
-if [[ $1 ]]; then
-	commitMessage = $1
+commitMessage='Auto Commit'
+if [ $# != 1 ] ; then
+	commitMessage=$1
 fi
 
+echo $commitMessage
+
 git add .
-git commit -m commitMessage
-git push
+git commit -m "$commitMessage"
+# git push
