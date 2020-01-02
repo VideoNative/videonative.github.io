634cf3341c1404d6c15546345f8981ac6bc5a84f
Norcy <chenyg32@gmail.com>
2020-01-02 19:34:45 +0800

    Auto Commit [by Norcy]

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -46,7 +46,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'VideoNative.github.io',
+  projectName: 'videonative.github.io',
   organizationName: 'VideoNative',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
