1b3e4ae4075d52d6bd179b65b723caa540c92e7e
Norcy <chenyg32@gmail.com>
2020-01-02 16:38:12 +0800

    Auto Commit [by Norcy]

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -40,13 +40,13 @@ const siteConfig = {
   title: 'Video Native', // Title for your website.
   tagline: 'Video Native Official Website',
   url: 'https://videonative.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/VideoNative.github.io/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'VideoNative',
+  projectName: 'VideoNative.github.io',
   organizationName: 'VideoNative',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
